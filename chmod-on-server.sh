f366cfff3e8f18740436201cc7ec3a985d4e649c
Wolfgang Huber <wolfgang.huber@embl.org>
2024-06-12 17:49:46 +0200

    Updated my personal page Fixed text colors, in a hacky way since I could not figure out the CSS Added code to fix the file permissions

---
+++ chmod-on-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+find /g/huber/www-huber/group -type d -exec chmod 775 {} \;
+find /g/huber/www-huber/group -type f -exec chmod 664 {} \;
+
--- upload-rsync.sh
+++ upload-rsync.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+Rscript fixcolors.R
 rsync -vrclpt _site/ datatransfer.embl.de:/g/huber/www-huber/group
+ssh datatransfer.embl.de 'bash -s' < ./chmod-on-server.sh
