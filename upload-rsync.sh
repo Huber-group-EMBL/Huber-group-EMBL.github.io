9d0a682942d09609bd663c19a3b255746c7606b5
Wolfgang Huber <wolfgang.huber@embl.org>
2024-07-18 13:42:40 +0200

    Doing permissions now locally, and using "-a" flag for rsync.

--- upload-rsync.sh
+++ upload-rsync.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 Rscript fixcolors.R
-rsync -vrclpt _site/ datatransfer.embl.de:/g/huber/www-huber/group
-ssh datatransfer.embl.de 'bash -s' < ./chmod-on-server.sh
+find _site -type d -exec chmod 775 {} \;
+find _site -type f -exec chmod 664 {} \;
+rsync -avz _site/ datatransfer.embl.de:/g/huber/www-huber/group
+
+# ssh datatransfer.embl.de 'bash -s' < ./chmod-on-server.sh
