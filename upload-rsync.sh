fffa7c34f2d7b12d06797b590752c84815e6b416
Wolfgang Huber <wolfgang.huber@embl.org>
2024-08-23 10:09:38 +0200

    using new theme

--- upload-rsync.sh
+++ upload-rsync.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-Rscript fixcolors.R
+#
+# Rscript fixcolors.R
+#
 find _site -type d -exec chmod 775 {} \;
 find _site -type f -exec chmod 664 {} \;
 rsync -avz _site/ datatransfer.embl.de:/g/huber/www-huber/group
 
+# old:
 # ssh datatransfer.embl.de 'bash -s' < ./chmod-on-server.sh
