b76e84a76919a26ee207c9b60d575ab3dfec7f32
Wolfgang Huber <wolfgang.huber@embl.org>
2025-01-21 21:24:19 +0100

    outdated

--- upload-rsync.sh
+++ upload-rsync.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #
-# Rscript fixcolors.R
+# *** This is no longer relevant, since the web server serves from an S3 bucket.
+# *** Please see  minio-README.md
 #
-find _site -type d -exec chmod 775 {} \;
-find _site -type f -exec chmod 664 {} \;
-rsync -avz _site/ datatransfer.embl.de:/g/huber/www-huber/group
+# find _site -type d -exec chmod 775 {} \;
+# find _site -type f -exec chmod 664 {} \;
+# rsync -avz _site/ datatransfer.embl.de:/g/huber/www-huber/group
 
-# old:
-# ssh datatransfer.embl.de 'bash -s' < ./chmod-on-server.sh
