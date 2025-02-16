af08e537d73f7075d2731453ee5bad4ba2890587
Wolfgang Huber <wolfgang.huber@embl.org>
2025-02-16 21:35:03 +0100

    fixed a link such that wget -spider does not complain about it.

--- checklinks.sh
+++ checklinks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget --spider -r -nd -nv -l 5 -o wget.log https://www.huber.embl.de/group/index.html
+wget --spider -r -nd -l 5 -o wget.log https://www.huber.embl.de/group/index.html
 
 # also follow external links:
 wget --spider -r -nd -nv -l 1 -H -o wget.log https://www.huber.embl.de/group/index.html
